4de3d8f1f35c7327e4620c482867c5bd29e2658b
Stephen Fox <stephen.j.fox.jr@gmail.com>
2024-01-16 21:10:44 -0500

    wgu: Added internal wgu library w/ wg key parsing.

---
+++ internal/wgu/keys.go
@@ -0,0 +1,51 @@
+package wgu
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"golang.org/x/crypto/curve25519"
+	"golang.zx2c4.com/wireguard/device"
+)
+
+func NewNoisePrivateKey() (*device.NoisePrivateKey, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return nil, err
+	}
+
+	private := &device.NoisePrivateKey{}
+	copy(private[:], b)
+	return private, nil
+}
+
+func NoisePublicKeyFromPrivate(private *device.NoisePrivateKey) *device.NoisePublicKey {
+	public := &device.NoisePublicKey{}
+	apk := (*[device.NoisePublicKeySize]byte)(public)
+	ask := (*[device.NoisePrivateKeySize]byte)(private)
+	curve25519.ScalarBaseMult(apk, ask)
+	return public
+}
+
+func NoisePrivateKeyFromBase64(b64 string) (*device.NoisePrivateKey, error) {
+	b, err := base64.StdEncoding.DecodeString(b64)
+	if err != nil {
+		return nil, err
+	}
+
+	private := &device.NoisePrivateKey{}
+	copy(private[:], b)
+	return private, nil
+}
+
+func NoisePublicKeyFromBase64(b64 string) (*device.NoisePublicKey, error) {
+	b, err := base64.StdEncoding.DecodeString(b64)
+	if err != nil {
+		return nil, err
+	}
+
+	public := &device.NoisePublicKey{}
+	copy(public[:], b)
+	return public, nil
+}
