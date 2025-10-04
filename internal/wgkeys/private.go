ff534a1360d79f390262a9ef58532e2be6304221
Stephen Fox <stephen.j.fox.jr@gmail.com>
2025-10-04 07:18:51 +0000

    wgkeys: Separate source files by key type

--- internal/wgkeys/private.go
+++ internal/wgkeys/private.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 
-	"golang.org/x/crypto/curve25519"
 	"golang.zx2c4.com/wireguard/device"
 )
 
@@ -20,14 +19,6 @@ func NewNoisePrivateKey() (*device.NoisePrivateKey, error) {
 	private := &device.NoisePrivateKey{}
 	copy(private[:], b)
 	return private, nil
-}
-
-func NoisePublicKeyFromPrivate(private *device.NoisePrivateKey) *device.NoisePublicKey {
-	public := &device.NoisePublicKey{}
-	apk := (*[device.NoisePublicKeySize]byte)(public)
-	ask := (*[device.NoisePrivateKeySize]byte)(private)
-	curve25519.ScalarBaseMult(apk, ask)
-	return public
 }
 
 func NoisePrivateKeyFromFilePath(filePath string) (*device.NoisePrivateKey, error) {
@@ -68,30 +59,4 @@ func WriteNoisePrivateKeyToFile(private *device.NoisePrivateKey, privateKeyFile 
 
 func NoisePrivateKeyToString(private *device.NoisePrivateKey) string {
 	return base64.StdEncoding.EncodeToString(private[:])
-}
-
-func NoisePrivateKeyToPublicDisplayString(private *device.NoisePrivateKey) string {
-	pub := NoisePublicKeyFromPrivate(private)
-
-	return base64.StdEncoding.EncodeToString(pub[:])
-}
-
-func NoisePublicKeyFromBase64(b64 string) (*device.NoisePublicKey, error) {
-	b, err := base64.StdEncoding.DecodeString(b64)
-	if err != nil {
-		return nil, err
-	}
-
-	return NoisePublicKeyFromBytes(b)
-}
-
-func NoisePublicKeyFromBytes(b []byte) (*device.NoisePublicKey, error) {
-	if len(b) != device.NoisePublicKeySize {
-		return nil, fmt.Errorf("got incorrect number of bytes (%d), expected: %d",
-			len(b), device.NoisePublicKeySize)
-	}
-
-	public := &device.NoisePublicKey{}
-	copy(public[:], b)
-	return public, nil
 }
---
+++ internal/wgkeys/public.go
@@ -0,0 +1,47 @@
+package wgkeys
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	"golang.org/x/crypto/curve25519"
+	"golang.zx2c4.com/wireguard/device"
+)
+
+func NoisePrivateKeyToPublicDisplayString(private *device.NoisePrivateKey) string {
+	pub := NoisePublicKeyFromPrivate(private)
+
+	return NoisePublicKeyToString(pub)
+}
+
+func NoisePublicKeyToString(pub *device.NoisePublicKey) string {
+	return base64.StdEncoding.EncodeToString(pub[:])
+}
+
+func NoisePublicKeyFromPrivate(private *device.NoisePrivateKey) *device.NoisePublicKey {
+	public := &device.NoisePublicKey{}
+	apk := (*[device.NoisePublicKeySize]byte)(public)
+	ask := (*[device.NoisePrivateKeySize]byte)(private)
+	curve25519.ScalarBaseMult(apk, ask)
+	return public
+}
+
+func NoisePublicKeyFromBase64(b64 string) (*device.NoisePublicKey, error) {
+	b, err := base64.StdEncoding.DecodeString(b64)
+	if err != nil {
+		return nil, err
+	}
+
+	return NoisePublicKeyFromBytes(b)
+}
+
+func NoisePublicKeyFromBytes(b []byte) (*device.NoisePublicKey, error) {
+	if len(b) != device.NoisePublicKeySize {
+		return nil, fmt.Errorf("got incorrect number of bytes (%d), expected: %d",
+			len(b), device.NoisePublicKeySize)
+	}
+
+	public := &device.NoisePublicKey{}
+	copy(public[:], b)
+	return public, nil
+}
